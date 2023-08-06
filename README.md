92cfc79167d529f3832d926a3b3061fca6773716
chimamedia <SHO.OOI@outlook.jp>
2023-08-07 07:33:07 +0900

    20230803
    Mode切り替えプログラム

--- README.md
+++ README.md
@@ -2,3 +2,15 @@
  
 # Refelence
 - [M5StickC-PlusにVSCodeのPlatformIOを利用して書き込む](https://qiita.com/ume-bar/items/234014927ae0857baf51)
+
+- [M5StickCによるジェスチャ認識(3)～Wi-Fi接続と学習データの生成](https://qiita.com/chimamedia/items/fd367f75c3979541c49d)
+
+-[【Arduino】M5StickCをWi-Fi接続する（１）](https://karakuri-musha.com/inside-technology/arduino-m5stickc-01-connect-wifi-for-ssid/)
+
+-[M5stickCではまった話とUDP通信](http://setsubi.no-mania.com/mstickv/m5stickc%E3%81%A7%E3%81%AF%E3%81%BE%E3%81%A3%E3%81%9F%E8%A9%B1%E3%81%A8udp%E9%80%9A%E4%BF%A1)
+
+-[【Arduino】ディスプレイ付きマイコン「M5StickC Plus」　Bluetooth（無線）でPCにデータを送信する！（Bluetooth Serial）](https://karakuri-musha.com/inside-technology/arduino-m5stickc-plus-10-bluetooth-serialimu-01/)
+
+-[M5StickC PLUSの加速度センサで振動測定と周波数特性](https://101010.fun/m5stickc-plus-accel.html)
+
+-[TonyMooori/pyserial_demo.py](https://gist.github.com/TonyMooori/412322f9aaa5ce9dd8e4)
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <M5StickCPlus.h>
-  
+#include <WiFi.h>
+#include <WiFiUDP.h>
+#include "time.h"
+
 #define MODE_A 0 // 数値表示
 #define MODE_B 1 // 姿勢表示
 #define MODE_C 2 // 波形表示
@@ -49,9 +52,14 @@ float gyroZ_dps = 0;
 
 //通信関係
 //WiFi
-#include <WiFi.h>
-const char* ssid = "yurucamp"; // ご自分のWi-FiルータのSSIDを記述します。 
-const char* password = "t7snicora4u"; // ご自分のWi-Fiルータのパスワードを記述します。
+const char* ssid = "ZZZZZZ"; // ご自分のWi-FiルータのSSIDを記述します。 
+const char* password = "ZZZZZZ"; // ご自分のWi-Fiルータのパスワードを記述します。
+const char* ntpServer =  "aaa.bbb.jp";
+const char* saddr = "XXX.XXX.XXX.XXX"; //taeget PC address
+const int sport = 55998;
+const int kport = 5556;
+uint8_t mac3[6];
+WiFiUDP Udp;
 
 //Bluethooth
 #include <BluetoothSerial.h>
@@ -108,6 +116,7 @@ void loop() {
       //M5.Lcd.setTextSize(2);
       //M5.Lcd.setRotation(1);  // ボタンBが上になる向き
     }else if(disp_mode == MODE_C){
+      M5.Lcd.setTextSize(1);
       disp_mode = MODE_D;
       
       // シリアルコンソールの開始　Start serial console.
@@ -130,8 +139,60 @@ void loop() {
         Serial.println("WiFi connected"); 
         Serial.println("IP address: "); 
         Serial.println(WiFi.localIP()); 
+
+        M5.Lcd.print(".");
       }
+      M5.Lcd.println("CONNECTED");
+      delay(1000);
+
+      // Set ntp time to local
+      configTime(9 * 3600, 0, ntpServer);
+      // Get local time
+      struct tm timeInfo;
+      if (getLocalTime(&timeInfo)) {
+        // Set RTC time
+        RTC_TimeTypeDef TimeStruct;
+        TimeStruct.Hours   = timeInfo.tm_hour;
+        TimeStruct.Minutes = timeInfo.tm_min;
+        TimeStruct.Seconds = timeInfo.tm_sec;
+        M5.Rtc.SetTime(&TimeStruct);
+
+        RTC_DateTypeDef DateStruct;
+        DateStruct.WeekDay = timeInfo.tm_wday;
+        DateStruct.Month = timeInfo.tm_mon + 1;
+        DateStruct.Date = timeInfo.tm_mday;
+        DateStruct.Year = timeInfo.tm_year + 1900;
+        M5.Rtc.SetData(&DateStruct);
+      }
+      M5.Lcd.fillScreen(BLACK);
+      M5.Lcd.setCursor(0, 1, 2);
+      M5.Lcd.print("Connected \nto ");
+      M5.Lcd.println(WiFi.localIP());
+
+      esp_read_mac(mac3, ESP_MAC_WIFI_STA);
+      M5.Lcd.printf("MAC: %02X:%02X:%02X:%02X:%02X:%02X\r\n", mac3[0], mac3[1], mac3[2], mac3[3], mac3[4], mac3[5]);
+      Udp.begin(kport); 
+
+      M5.Lcd.setTextSize(2);
     }else if(disp_mode == MODE_D){
+      if (WiFi.status() == WL_CONNECTED) {
+        //Wi-Fiからの切断
+        Serial.println("Disconnect from WiFi"); 
+        WiFi.disconnect();
+
+        // Wi-Fi接続の状況を監視（WiFi.statusがWL_DISCONNECTEDになるまで繰り返し
+        while (WiFi.status() !=WL_DISCONNECTED) {
+          delay(500);
+          Serial.print(".");
+          M5.Lcd.print(".");
+        }
+
+        // Wi-Fi切断結果をシリアルモニタへ出力
+        Serial.println("");
+        Serial.println("WiFi disconnected");
+        M5.Lcd.print("WiFi disconnected");
+      }
+      M5.Lcd.setTextSize(2);
       disp_mode = MODE_E;
     }else{
       disp_mode = MODE_A;
@@ -229,27 +290,67 @@ void loop() {
     delay(SAMPLE_PERIOD);
 
     i++;
-
-    /*
-    for(int i = 0; i < SAMPLE_SIZE; ++i)
-    {
-      M5.IMU.getAccelData(&ax,&ay,&az[i]);  // IMUから加速度を取得
-  
-      az[i] *= 1000;  // mGに変換
-      if (i == 0) continue;
-      
-//        Serial.println(az[i]); // シリアルモニタは115200baudで通信
-  
-      int y0 = map((int)(az[i - 1]), MINZ, MAXZ, M5.Lcd.height(), 0);
-      int y1 = map((int)(az[i]), MINZ, MAXZ, M5.Lcd.height(), 0);
-      M5.Lcd.drawLine(i - 1 + X0, y0, i + X0, y1, YELLOW);
-      delay(SAMPLE_PERIOD);
-    }
-    */
   }else if(disp_mode == MODE_D){
-    
-  }else if(disp_mode == MODE_E){
+    // 現在の姿勢を検出する
+    if(near_zero(accX_mpss) && near_p_g(accY_mpss) && near_zero(accZ_mpss)){
+      pose = POSE_P_Y;
+    }else if(near_p_g(accX_mpss) && near_zero(accY_mpss) && near_zero(accZ_mpss)){
+      pose = POSE_P_X;
+    }else if(near_zero(accX_mpss) && near_zero(accY_mpss) && near_p_g(accZ_mpss)){
+      pose = POSE_P_Z;
+    }else if(near_zero(accX_mpss) && near_m_g(accY_mpss) && near_zero(accZ_mpss)){
+      pose = POSE_M_Y;
+    }else if(near_m_g(accX_mpss) && near_zero(accY_mpss) && near_zero(accZ_mpss)){
+      pose = POSE_M_X;
+    }else if(near_zero(accX_mpss) && near_zero(accY_mpss) && near_m_g(accZ_mpss)){
+      pose = POSE_M_Z;
+    }
 
+    // 姿勢に変化があった場合にのみ描画する
+    if(prev_pose != pose){
+      M5.Lcd.fillScreen(BLACK);
+      switch(pose){
+      case POSE_P_X:
+        M5.Lcd.setRotation(1);
+        M5.Lcd.setCursor(56, 16);
+        M5.Lcd.print("+X");
+        break;
+      case POSE_M_X:
+        M5.Lcd.setRotation(3);
+        M5.Lcd.setCursor(56, 16);
+        M5.Lcd.print("-X");
+        break;
+      case POSE_P_Y:
+        M5.Lcd.setRotation(0);
+        M5.Lcd.setCursor(16, 56);
+        M5.Lcd.print("+Y");
+        break;
+      case POSE_M_Y:
+        M5.Lcd.setRotation(2);
+        M5.Lcd.setCursor(16,56);
+        M5.Lcd.print("-Y");
+        break;
+      case POSE_P_Z:
+        M5.Lcd.setRotation(1);
+        M5.Lcd.setCursor(56, 16);
+        M5.Lcd.print("+Z");
+        break;
+      case POSE_M_Z:
+        M5.Lcd.setRotation(1);
+        M5.Lcd.setCursor(56, 16);
+        M5.Lcd.print("-Z");
+        break;
+      default:
+        ;
+      }
+      //UDP
+      Udp.beginPacket(saddr, sport);
+      Udp.printf("%d", pose);
+      Udp.endPacket();  
+      delay(100);
+    }
+  }else if(disp_mode == MODE_E){
+    ;
   }
 
   delay(100);
